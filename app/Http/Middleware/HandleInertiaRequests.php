4c180400d853a325b61d851a03bb67a39cbdcc20
voltakits <pertamake60gmail.com>
2026-07-08 19:15:55 +0700

    Fix data user di frontend

--- app/Http/Middleware/HandleInertiaRequests.php
+++ app/Http/Middleware/HandleInertiaRequests.php
@@ -37,7 +37,7 @@ class HandleInertiaRequests extends Middleware
     {
         return array_merge(parent::share($request), [
             'auth' => [
-                'user' => $request->user() ? $request->user()->only('id', 'name', 'email', 'role') : null,
+                'user' => $request->user() ? $request->user()->only('id', 'name', 'email', 'role', 'phone', 'avatar') : null,
             ],
             'flash' => [
                 'message' => fn () => $request->session()->get('message')
